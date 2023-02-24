2cc91186f5313e36810b591045fffebd829113fe
MalakhovSky <tviler6@gmail.com>
2023-02-24 13:59:19 +0300

    add storybook, loki

---
+++ config/storybook/preview.js
@@ -0,0 +1,19 @@
+import { addDecorator } from '@storybook/react';
+import { styleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecarator/RouterDecarator';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(styleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
